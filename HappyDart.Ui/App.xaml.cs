c12ba28e0e39745c776b082ef58e713cce525215
Mateusz <mateuszgra89@gmail.com>
2024-07-08 21:44:40 +0200

    Init commit

---
+++ HappyDart.Ui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HappyDart.Ui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
