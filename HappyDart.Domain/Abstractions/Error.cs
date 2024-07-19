ea58c3ab36e83c0ddd23c96d6dfa43d6dc34b000
Mateusz <mateuszgra89@gmail.com>
2024-07-19 08:57:55 +0200

    domain - add abstractions

---
+++ HappyDart.Domain/Abstractions/Error.cs
@@ -0,0 +1,9 @@
+﻿namespace HappyDart.Domain.Abstractions
+{
+    public record Error(string Code, string Name)
+    {
+        public static readonly Error None = new Error(string.Empty, string.Empty);
+
+        public static readonly Error NullValue = new Error("Error.NullValue", "Null value was provided.");
+    }
+}
