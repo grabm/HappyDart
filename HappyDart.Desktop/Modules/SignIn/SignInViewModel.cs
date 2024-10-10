d6a761bce1a1fa6a51e07d616bc1a8a1ae794755
Mateusz <mateuszgra89@gmail.com>
2024-10-10 23:23:21 +0200

    desktop - set test button property

--- HappyDart.Desktop/Modules/SignIn/SignInViewModel.cs
+++ HappyDart.Desktop/Modules/SignIn/SignInViewModel.cs
@@ -1,9 +1,22 @@
-﻿using System.ComponentModel;
+﻿using HappyDart.Desktop.Commands;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
 
 namespace HappyDart.Desktop.Modules.SignIn
 {
     public class SignInViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand SignInButtonCommand { get; }
+        public SignInViewModel()
+        {
+            SignInButtonCommand = new RelayCommand(ExecudeLogIn);
+        }
+
+        public void ExecudeLogIn()
+        {
+            MessageBox.Show("kliknąłem ExecudeLogIn");
+        }
     }
 }
